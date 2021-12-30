e11316d1adf5f2cf43645a2dc68fc8fa5da2255d
hoangthach1402 <vuhoangthach1402@gmail.com>
2021-12-30 14:31:34 +0700

    .

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,5 +4,28 @@ const router = express.Router()
 router.get('/', (req, res) => {
   res.send('get all bootcamps')
 })
-
+router.get('/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `show bootcapms ${req.params.id}`,
+  })
+})
+router.post('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `post bootcapms`,
+  })
+})
+router.put('/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `show bootcapms ${req.params.id}`,
+  })
+})
+router.delete('/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `delete bootcapms ${req.params.id}`,
+  })
+})
 module.exports = router
